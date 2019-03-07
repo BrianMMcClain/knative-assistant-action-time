e2f814b4b291224f4b45d50c8f5a25b8ece6eabb
Brian McClain <brianmmcclain@gmail.com>
2019-03-07 10:19:38 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+module.exports = x => {
+    d = new Date()
+
+    var ampm = "AM";
+    var hour = d.getHours();
+    if (hour > 12) {
+        hour -= 12;
+        ampm = "PM";
+    }
+
+    var minute = d.getMinutes();
+    if (minute < 10) {
+        minute = `0${minute}`;
+    }
+
+    return `The current time is ${hour} ${minute} ${ampm}`
+}
